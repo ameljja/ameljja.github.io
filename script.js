bf276770a435496c5c768c127d291e3aed4815b2
ameljja <40388179+ameljja@users.noreply.github.com>
2019-05-23 10:41:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const width = window.innerWidth;
+const p = document.querySelector("p");
+
+const getPercentX = x => Math.round((x / width) * 360);
+const styleEl = document.documentElement.style;
+const setStyle = styleEl.setProperty.bind(styleEl);
+
+document.addEventListener("click", e => {
+    const gradStart = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+    const gradEnd = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+
+    setStyle("--grad-start", gradStart);
+    setStyle("--grad-end", gradEnd);
+    p.setAttribute("data-gradStart", gradStart + "  |  ");
+    p.setAttribute("data-gradEnd", gradEnd);
+});
