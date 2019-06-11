dd0a466fbf351428541efb4a87bc5a0769d3f9aa
ameljja <40388179+ameljja@users.noreply.github.com>
2019-06-11 14:45:31 +0200

    Deleted const variable from script.js and letter-spacing from style.scss. Added letter-spacing to &::after.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const width = window.innerWidth;
 const p = document.querySelector("p");
 
-const getPercentX = x => Math.round((x / width) * 360);
 const styleEl = document.documentElement.style;
 const setStyle = styleEl.setProperty.bind(styleEl);
 
-document.addEventListener("click", (e) => {
+document.addEventListener("click", e => {
     const gradStart = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
     Math.random() * 256
   )}, ${Math.floor(Math.random() * 256)})`;
